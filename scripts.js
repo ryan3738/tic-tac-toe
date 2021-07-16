b122f6f2dab31b0491756138450e9ca0399c3f87
Ryan <ryan3738@gmail.com>
2021-07-16 15:00:14 -0700

    integrate localStorage into app

--- scripts.js
+++ scripts.js
@@ -10,31 +10,54 @@ const addBookButton = document.getElementById('addBookButton')
 const span = document.getElementById('closeModal')
 const submitNewBookButton = document.getElementById('submitNewBookButton')
 
-const myLibrary = [
-  { id: 0, title: 'The Hobbit', author: 'Tolkien', pages: 254, haveRead: true },
+const myLibrary = []
+
+function getRandomInt(minParam, maxParam) {
+  const min = Math.ceil(minParam)
+  const max = Math.floor(maxParam)
+  return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+}
+
+console.log('modal', modal)
+
+const starterLibrary = [
+  {
+    id: 0,
+    title: 'The Hobbit',
+    author: 'Tolkien',
+    pages: 310,
+    haveRead: true,
+  },
   {
     id: 1,
     title: 'Lord of The Rings: Return of the King',
     author: 'Tolkien',
-    pages: 254,
+    pages: 416,
     haveRead: true,
   },
   {
     id: 2,
-    title: 'The Hobbit',
-    author: 'Tolkien',
-    pages: 254,
+    title: 'A Game of Thrones',
+    author: 'George R. R. Martin',
+    pages: 694,
     haveRead: false,
   },
 ]
 
+// console.log(localStorage.getItem('myLibrary').length);
+
+// myLibrary.push(JSON.parse(initializeLibrary()))
+
+// localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+
 function getId() {
   // Create function that checks if id exists and assigns next availabe value
-}
-
-function addSomeBooks() {
-  // Create function to populate some books for testing
-  const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
+  const arr = myLibrary
+  let id = 0
+  while (arr.some((el) => el.id === id)) {
+    id += 1
+  }
+  return id
 }
 
 // 1. Make event listener for button to show modal for new book info
@@ -64,9 +87,6 @@ function Book(id, title, author, pages, haveRead) {
   this.author = author
   this.pages = pages
   this.haveRead = haveRead
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${haveRead}`
-  }
 }
 
 function putBooksAway() {
@@ -94,6 +114,7 @@ function displayBooks() {
     const toggleReadButton = document.createElement('button')
     const removeBookButton = document.createElement('button')
 
+    // Toggle read button
     toggleReadButton.id = 'readButton'
     toggleReadButton.title = 'Read'
     toggleReadButton.innerHTML = 'Read'
@@ -105,11 +126,13 @@ function displayBooks() {
       toggleReadButton.className = 'haveNotRead'
     }
 
+    // Remove book button
     removeBookButton.innerHTML = 'X'
     removeBookButton.title = 'Remove Book'
     removeBookButton.id = 'deleteBookButton'
     removeBookButton.addEventListener('click', removeBook)
 
+    // Make the book card
     bookCard.className = 'bookCard'
     bookCard.id = eachBook.id
     bookTitle.innerHTML = eachBook.title
@@ -124,8 +147,34 @@ function displayBooks() {
   }
 }
 
+// Book.prototype.sayName = function () {
+//   console.log(this.title)
+// }
+
+// Function to update library when adding books
+const updateLibrary = function (bookObject) {
+  myLibrary.push(bookObject)
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+  console.log(myLibrary)
+  displayBooks()
+}
+
+function addSomeBooks() {
+  // Create function to populate some books for testing
+  for (const book of starterLibrary) {
+    const starterBook = new Book(
+      book.id,
+      book.title,
+      book.author,
+      book.pages,
+      book.haveRead
+    )
+    updateLibrary(starterBook)
+  }
+}
+
 // 4. Allow user to change read status of books on button press
-function toggleHaveRead() {
+const toggleHaveRead = function () {
   // Toggle read status of current book
   const bookCardId = parseInt(this.parentElement.id)
   const index = myLibrary.findIndex((book) => book.id === bookCardId)
@@ -136,6 +185,7 @@ function toggleHaveRead() {
     ...myLibrary[index],
     haveRead: !currentStatus,
   }
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
   displayBooks()
 }
 
@@ -143,21 +193,20 @@ function toggleHaveRead() {
 function removeBook() {
   const bookCardId = parseInt(this.parentElement.id)
   const index = myLibrary.findIndex((book) => book.id === bookCardId)
-  console.log('index...', index)
+  console.log('index:', index)
   myLibrary.splice(index, 1)
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
   displayBooks()
 }
 
 // 6. Store users library in local storage
-
 // Run displayBook function on window load
 // window.onload = displayBook
-displayBooks()
+// displayBooks()
 
-function addBookToLibrary(form) {
-  // do stuff here
-  const id = myLibrary.length
-  let bookObject = new Book(id)
+function formToObject(form) {
+  const bookObject = {}
+  console.log(bookObject)
   for (const eachInput of form) {
     let { name, value, type } = eachInput
     if (type === 'number') {
@@ -167,15 +216,28 @@ function addBookToLibrary(form) {
       value = eachInput.checked
     }
     if (value !== undefined && type !== 'submit' && type !== 'fieldset') {
-      bookObject = {
-        ...bookObject,
-        [name]: value,
-      }
+      bookObject[name] = value
     }
   }
-  myLibrary.push(bookObject)
-  console.log(myLibrary)
-  displayBooks()
+  return bookObject
+}
+
+function addBookToLibrary(form) {
+  // do stuff here
+  const id = getId()
+  const formObject = formToObject(form)
+  const bookObject = new Book(
+    id,
+    formObject.title,
+    formObject.author,
+    formObject.pages,
+    formObject.haveRead
+  )
+  updateLibrary(bookObject)
+}
+
+function randomizeForm(){
+  
 }
 
 // 2.1 Allow user to submit book info in modal
@@ -185,3 +247,18 @@ submitNewBookButton.onclick = function (event) {
   const { form } = event.target
   addBookToLibrary(form)
 }
+
+function initializeLibrary() {
+  if (!localStorage.getItem('myLibrary')) {
+    // Remove later once starter library is not needed.
+    return addSomeBooks()
+  }
+  if (localStorage.getItem('myLibrary')) {
+    return myLibrary.push(...JSON.parse(localStorage.getItem('myLibrary')))
+  }
+}
+
+console.log('localStorage', JSON.parse(localStorage.getItem('myLibrary')))
+
+initializeLibrary()
+displayBooks()
