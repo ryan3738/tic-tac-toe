c49ee0dc5dad4048ee8fb4bf8b4da9649c5f8565
Ryan <ryan3738@gmail.com>
2021-07-21 12:11:09 -0700

    initiate factory functions

--- instructions.md
+++ instructions.md
@@ -1,24 +1,12 @@
-1.  If you haven’t already, set up your project with skeleton HTML/CSS and JS files.
-2.  All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. Your code should look something like this:
-    
-        let myLibrary = [];
-        
-        function Book() {
-          // the constructor...
-        }
-        
-        function addBookToLibrary() {
-          // do stuff here
-        }
-        
-    
-3.  Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
-4.  Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
-5.  Add a button on each book’s display to remove the book from the library.
-    1.  You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
-6.  Add a button on each book’s display to change its `read` status.
-    1.  To facilitate this you will want to create the function that toggles a book’s `read` status on your `Book` prototype instance.
-7.  Optional - we haven’t learned any techniques for actually storing our data anywhere, so when the user refreshes the page, all of their books will disappear! If you want, you are capable of adding some persistence to this library app using the Web Storage API.
-    1.  `localStorage` ([docs here](https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API)) allows you to save data on the user’s computer. The downside here is that the data is ONLY accessible on the computer that it was created on. Even so, it’s pretty handy! Set up a function that saves the whole library array to localStorage every time a new book is created, and another function that looks for that array in localStorage when your app is first loaded. Additionally, here are a couple of quick tips to help you not get tripped up:
-        *   Make sure your app doesn’t crash if the array you retrieve from localStorage isn’t there!
-        *   localStorage uses [JSON](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON) to send and store data, and when you retrieve the data, it will also be in JSON format. You will learn more about this language in a later lesson, but it doesn’t hurt to get your feet wet now. Keep in mind you _cannot store functions in JSON_, so you’ll have to figure out how to add methods back to your object properties once you fetch them. Good luck!
+1.  Set up your project with a HTML, CSS and Javascript files and get the Git repo all set up.
+2.  You’re going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects… and you’re probably going to want an object to control the flow of the game itself.
+    1.  Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory. Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
+3.  Set up your HTML and write a JavaScript function that will render the contents of the gameboard array to the webpage (for now you can just manually fill in the array with `"X"`s and `"O"`s)
+4.  Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker. Don’t forget the logic that keeps players from playing in spots that are already taken!
+    1.  Think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects.. but take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
+5.  Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
+6.  Clean up the interface to allow players to put in their names, include a button to start/restart the game and add a display element that congratulates the winning player!
+7.  Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
+    1.  Start by just getting the computer to make a random legal move.
+    2.  Once you’ve gotten that, work on making the computer smart. It is possible to create an unbeatable AI using the minimax algorithm (read about it [here](https://en.wikipedia.org/wiki/Minimax), some googling will help you out with this one)
+    3.  If you get this running _definitely_ come show it off in the chatroom. It’s quite an accomplishment!
--- scripts.js
+++ scripts.js
@@ -1,269 +1,31 @@
-const container = document.querySelector('#container')
+const gameBoard = document.querySelector('#gameBoard')
 
-// Get the modal
-const modal = document.getElementById('newBookModal')
+const gameBoard = (() => {
+    const gameBoardArray = []
+    const add = (a, b) => a + b;
 
-// Get the button that opens the modal
-const addBookButton = document.getElementById('addBookButton')
-const newBookForm = document.getElementById('newBookForm')
+    return {
+        add,
 
-// Get the <span> element that closes the modal
-const span = document.getElementById('closeModal')
-const submitNewBookButton = document.getElementById('submitNewBookButton')
+    };
+})();
 
-const myLibrary = []
+const displayController = (() => {
+    const gameBoardArray = []
 
-function getRandomInt(minParam, maxParam) {
-  const min = Math.ceil(minParam)
-  const max = Math.floor(maxParam)
-  return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
-}
+    return {    };
+})();
 
-console.log('newBookForm', newBookForm)
+const playGame = (() => {
+    const gameBoardArray = []
 
-const starterLibrary = [
-  {
-    id: 0,
-    title: 'The Hobbit',
-    author: 'Tolkien',
-    pages: 310,
-    haveRead: true,
-  },
-  {
-    id: 1,
-    title: 'Lord of The Rings: Return of the King',
-    author: 'Tolkien',
-    pages: 416,
-    haveRead: true,
-  },
-  {
-    id: 2,
-    title: 'A Game of Thrones',
-    author: 'George R. R. Martin',
-    pages: 694,
-    haveRead: false,
-  },
-]
+    return {    };
+})();
 
-// console.log(localStorage.getItem('myLibrary').length);
-
-// myLibrary.push(JSON.parse(initializeLibrary()))
-
-// localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-
-function getId() {
-  // Create function that checks if id exists and assigns next availabe value
-  const arr = myLibrary
-  let id = 0
-  while (arr.some((el) => el.id === id)) {
-    id += 1
-  }
-  return id
-}
-
-// 1. Make event listener for button to show modal for new book info
-
-// When the user clicks on the button, open the modal
-addBookButton.onclick = function () {
-  modal.style.display = 'block'
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none'
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = 'none'
-  }
-}
-
-// Book constructor function
-function Book(id, title, author, pages, haveRead) {
-  // the constructor...
-  this.id = id
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.haveRead = haveRead
-}
-
-function putBooksAway() {
-  console.log('Cleaning up old books...')
-  const oldBooks = container.querySelectorAll('div')
-  oldBooks.forEach((book) => {
-    if (book.id !== 'notBook') {
-      book.remove()
-    }
-  })
-  console.log('Bye bye old books...')
-}
-
-// Function that loops through the array and displays each book on the page.
-// 3. On myLibrary change update books that are displayed
-function displayBooks() {
-  // Clean up old books from display before displaying library
-  putBooksAway()
-  // Cycle through books in library and create a card for each book
-  for (const eachBook of myLibrary) {
-    const bookCard = document.createElement('div')
-    const bookTitle = document.createElement('h2')
-    const bookAuthor = document.createElement('p')
-    const bookPages = document.createElement('p')
-    const toggleReadButton = document.createElement('button')
-    const removeBookButton = document.createElement('button')
-
-    // Toggle read button
-    toggleReadButton.id = 'readButton'
-    toggleReadButton.title = 'Read'
-    toggleReadButton.innerHTML = 'Read'
-    toggleReadButton.addEventListener('click', toggleHaveRead)
-    if (eachBook.haveRead) {
-      toggleReadButton.className = 'haveRead'
-    }
-    if (!eachBook.haveRead) {
-      toggleReadButton.className = 'haveNotRead'
-    }
-
-    // Remove book button
-    removeBookButton.innerHTML = 'X'
-    removeBookButton.title = 'Remove Book'
-    removeBookButton.id = 'deleteBookButton'
-    removeBookButton.addEventListener('click', removeBook)
-
-    // Make the book card
-    bookCard.className = 'bookCard'
-    bookCard.id = eachBook.id
-    bookTitle.innerHTML = eachBook.title
-    bookCard.appendChild(bookTitle)
-    bookAuthor.innerHTML = eachBook.author
-    bookCard.appendChild(bookAuthor)
-    bookPages.innerHTML = eachBook.pages
-    bookCard.appendChild(bookPages)
-    bookCard.appendChild(toggleReadButton)
-    bookCard.appendChild(removeBookButton)
-    container.insertBefore(bookCard, container.childNodes[0])
-  }
-}
-
-// Book.prototype.sayName = function () {
-//   console.log(this.title)
-// }
-
-// Function to update library when adding books
-const updateLibrary = function (bookObject) {
-  myLibrary.push(bookObject)
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-  console.log(myLibrary)
-  displayBooks()
-}
-
-function addSomeBooks() {
-  // Create function to populate some books for testing
-  for (const book of starterLibrary) {
-    const starterBook = new Book(
-      book.id,
-      book.title,
-      book.author,
-      book.pages,
-      book.haveRead
-    )
-    updateLibrary(starterBook)
-  }
-}
-
-// 4. Allow user to change read status of books on button press
-const toggleHaveRead = function () {
-  // Toggle read status of current book
-  const bookCardId = parseInt(this.parentElement.id)
-  const index = myLibrary.findIndex((book) => book.id === bookCardId)
-  console.log('index...', index)
-  const currentStatus = myLibrary[index].haveRead
-  console.log(currentStatus)
-  myLibrary[index] = {
-    ...myLibrary[index],
-    haveRead: !currentStatus,
-  }
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-  displayBooks()
-}
-
-// 5. Allow user to delete book from library on button press
-function removeBook() {
-  const bookCardId = parseInt(this.parentElement.id)
-  const index = myLibrary.findIndex((book) => book.id === bookCardId)
-  console.log('index:', index)
-  myLibrary.splice(index, 1)
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-  displayBooks()
-}
-
-// 6. Store users library in local storage
-// Run displayBook function on window load
-// window.onload = displayBook
-// displayBooks()
-
-function formToObject(form) {
-  const bookObject = {}
-  console.log(bookObject)
-  for (const eachInput of form) {
-    let { name, value, type } = eachInput
-    if (type === 'number') {
-      value = parseInt(value)
-    }
-    if (type === 'checkbox') {
-      value = eachInput.checked
-    }
-    if (value !== undefined && type !== 'submit' && type !== 'fieldset') {
-      bookObject[name] = value
-    }
-  }
-  return bookObject
-}
-
-function addBookToLibrary(form) {
-  // do stuff here
-  const id = getId()
-  const formObject = formToObject(form)
-  const bookObject = new Book(
-    id,
-    formObject.title,
-    formObject.author,
-    formObject.pages,
-    formObject.haveRead
-  )
-  updateLibrary(bookObject)
-}
-
-function randomizeForm() {
-  const randomBook = starterLibrary[getRandomInt(0, starterLibrary.length)]
-  for (const each of newBookForm) {
-    each.value = randomBook[each.name]
-  }
-}
-
-// 2.1 Allow user to submit book info in modal
-// 2.2 Hide modal after user submits new book?
-submitNewBookButton.onclick = function (event) {
-  event.preventDefault()
-  const { form } = event.target
-  addBookToLibrary(form)
-  randomizeForm()
-}
-
-function initializeLibrary() {
-  if (!localStorage.getItem('myLibrary')) {
-    // Remove later once starter library is not needed.
-    return addSomeBooks()
-  }
-  if (localStorage.getItem('myLibrary')) {
-    return myLibrary.push(...JSON.parse(localStorage.getItem('myLibrary')))
-  }
-}
-
-console.log('localStorage', JSON.parse(localStorage.getItem('myLibrary')))
-
-initializeLibrary()
-displayBooks()
+const Player = (name, level) => {
+    let health = level * 2;
+    const getLevel = () => level;
+    const getName = () => name;
+    const die = () => {
+        // uh oh
+    };
