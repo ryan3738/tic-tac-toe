97cd7160c760505f9f02583406044b804df32003
Ryan <ryan3738@gmail.com>
2021-07-27 11:02:22 -0700

    eslint setup and fix

--- scripts.js
+++ scripts.js
@@ -1,68 +1,63 @@
-const gameBoards = document.querySelector('#gameBoard')
-const gridBoxes = gameBoards.querySelectorAll('div')
-
+const gameBoards = document.querySelector('#gameBoard');
+const gridBoxes = gameBoards.querySelectorAll('div');
 
 const gameBoard = (() => {
-    const gameBoardArray = []
-    // Create function to play a position
-    const selectPosition = (position, marker) => {
-        const positionInt = parseInt(position)
+  const gameBoardArray = [];
+  // Create function to play a position
+  const selectPosition = (position, marker) => {
+    const positionInt = parseInt(position);
 
-        if (gameBoardArray.some((gridBox) => gridBox.id === positionInt)){
-            return
-        }
-
-        if(!gameBoardArray.position){
-            gameBoardArray.push({ id: positionInt, value: marker })
-            const gridBox = document.getElementById(`${position}`)
-            gridBox.innerHTML = `${marker}`
-            // console.log('Game Board:',gameBoardArray);
-        }
+    if (gameBoardArray.some((gridBox) => gridBox.id === positionInt)) {
+      return;
     }
 
+    if (!gameBoardArray.position) {
+      gameBoardArray.push({ id: positionInt, value: marker });
+      const gridBox = document.getElementById(`${position}`);
+      gridBox.innerHTML = `${marker}`;
+      // console.log('Game Board:',gameBoardArray);
+    }
+  };
 
-    const win = () => {
-
-}
-    return {
-        selectPosition
-    };
+  const win = () => {};
+  return {
+    selectPosition,
+  };
 })();
 
 const displayController = (() => {
-    const gameBoardArray = []
+  const gameBoardArray = [];
 
-    return {    };
+  return {};
 })();
 
 const playGame = (() => {
-    const gameBoardArray = []
+  const gameBoardArray = [];
 
-    return {    };
+  return {};
 })();
 
 const Player = (name, level) => {
-    let health = level * 2;
-    const getLevel = () => level;
-    const getName = () => name;
-    const die = () => {
-        // uh oh
-    };
-}
+  const health = level * 2;
+  const getLevel = () => level;
+  const getName = () => name;
+  const die = () => {
+    // uh oh
+  };
+};
 
 function filterKeyPress(value) {
-    // console.log(this);
-    if (this.className === "gridBox"){
-        // console.log('this', this);
-        gameBoard.selectPosition(this.id, 'X')
-    }
-
+  // console.log(this);
+  if (this.className === 'gridBox') {
+    // console.log('this', this);
+    gameBoard.selectPosition(this.id, 'X');
+  }
 }
+
 // console.log(gameBoard.selectPosition(1, 'X'));
 
 gridBoxes.forEach((gridBox) => {
-    console.log('gridBox:',gridBox);
-    // console.log('this:',this);
-    gridBox.addEventListener('click', filterKeyPress)
-})
-
+  console.log('gridBox:', gridBox);
+  // console.log('this:',this);
+  gridBox.addEventListener('click', filterKeyPress);
+});
